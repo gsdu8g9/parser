841a9d043bfe1756666eba4ce18b744c9f3f6afe
nejtr0n <a6y@xakep.ru>
2015-09-01 12:41:45 +0300

    Update Filter.php
    Force utf8 to all sites responses

--- src/Filter.php
+++ src/Filter.php
@@ -20,7 +20,12 @@ abstract class Filter implements \Parser\Interfaces\Filter {
 
     public function filter(\RollingCurl\Request $data) {
         //var_dump($data);
-        preg_match($this->__regexp, $data->getResponseText(), $match);
+        preg_match ('/text\/html; charset=(.+)/i', $data->getResponseInfo()["content_type"], $enc);
+        if (!empty($enc[1])) {
+            preg_match($this->__regexp, iconv($enc[1], 'UTF-8', $data->getResponseText()), $match); // Force UTF-8
+        } else {
+            preg_match($this->__regexp, $data->getResponseText(), $match);
+        }
         return (!empty($match[1])) ? $match[1] : NULL;
     }
 }
